f09c912ea3467a16687553e4968e1b9dd32bfa15
OTA Hiroshi <otahiros@gmail.com>
2013-05-19 14:45:57 +0900

    does refactoring rspec with around.

--- traffic-monitor/spec/traffic-monitor_spec.rb
+++ traffic-monitor/spec/traffic-monitor_spec.rb
@@ -3,44 +3,34 @@ require File.join( File.dirname( __FILE__ ), "spec_helper" )
 require "traffic-monitor"
 
 describe TrafficMonitor do
+  around do | example |
+    network {
+      vswitch ( "tmsw" )  { datapath_id "0xabc" }
+      vhost   ( "host1" ) { mac "00:00:00:01:00:01" }
+      vhost   ( "host2" ) { mac "00:00:00:01:00:02" }
+      link "tmsw", "host1"
+      link "tmsw", "host2"
+    }.run( TrafficMonitor ) {
+      example.run
+    }
+  end
+
   it "transfers all packets" do
-    network {
-      vswitch ( "tmsw" )  { datapath_id "0xabc" }
-      vhost   ( "host1" ) { mac "00:00:00:01:00:01" }
-      vhost   ( "host2" ) { mac "00:00:00:01:00:02" }
-      link "tmsw", "host1"
-      link "tmsw", "host2"
-    }.run( TrafficMonitor ) {
-      send_packets "host1", "host2",  :n_pkts => 10, :pps => 10
-      sleep 1
-      vhost( "host2" ).stats( :rx ).should have( 10 ).packets
-    }
+    send_packets "host1", "host2",  :n_pkts => 10, :pps => 10
+    sleep 1
+    vhost( "host2" ).stats( :rx ).should have( 10 ).packets
   end
+
   it "counts sent packets" do
-    network {
-      vswitch ( "tmsw" )  { datapath_id "0xabc" }
-      vhost   ( "host1" ) { mac "00:00:00:01:00:01" }
-      vhost   ( "host2" ) { mac "00:00:00:01:00:02" }
-      link "tmsw", "host1"
-      link "tmsw", "host2"
-    }.run( TrafficMonitor ) {
-      send_packets "host1", "host2",  :n_pkts => 10, :pps => 10
-      sleep 1
-      controller("TrafficMonitor").get_packet_count( "00:00:00:01:00:01" ).should == 10
-    }
+    send_packets "host1", "host2",  :n_pkts => 10, :pps => 10
+    sleep 1
+    controller("TrafficMonitor").get_packet_count( "00:00:00:01:00:01" ).should == 10
   end
+
   it "counts sent packet sizes" do
-    network {
-      vswitch ( "tmsw" )  { datapath_id "0xabc" }
-      vhost   ( "host1" ) { mac "00:00:00:01:00:01" }
-      vhost   ( "host2" ) { mac "00:00:00:01:00:02" }
-      link "tmsw", "host1"
-      link "tmsw", "host2"
-    }.run( TrafficMonitor ) {
-      send_packets "host1", "host2",  :n_pkts => 10, :pps => 10
-      sleep 1
-      controller("TrafficMonitor").get_packet_size( "00:00:00:01:00:01" ).should == 640
-    }
+    send_packets "host1", "host2",  :n_pkts => 10, :pps => 10
+    sleep 1
+    controller("TrafficMonitor").get_packet_size( "00:00:00:01:00:01" ).should == 640
   end
 end
 
